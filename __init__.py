6622293592502efdef32bc80a7ae6e46dd2596d5
iancynk <ian.cynk@posteo.eu>
2019-01-24 17:35:32 +0100

    Added preferences to change the format of the input zotero toxt, so far implemented betterbibtexkey and bibliography

--- __init__.py
+++ __init__.py
@@ -12,7 +12,6 @@ from zim.applications import Application, ApplicationError
 from zim.gui.widgets import Dialog, Button, InputEntry, ScrolledWindow
 import json
 import urllib2
-# import requests
 
 
 class ZoteroPlugin(PluginClass):
@@ -41,7 +40,8 @@ class ZoteroPlugin(PluginClass):
 
     plugin_preferences = (
         ('bibliography_style', 'choice', _('Bibliography Style'),
-         'bbtkey', ('bbtkey', 'easykey', 'key', 'bibliography')),
+         'betterbibtexkey',
+         ('betterbibtexkey', 'easykey', 'key', 'bibliography')),
     )
 
 
@@ -53,7 +53,7 @@ class MainWindowExtension(WindowExtension):
         <menubar name='menubar'>
             <menu action='insert_menu'>
                 <placeholder name='plugin_items'>
-                    <menuitem action='insert_citation_bbt'/>
+                    <menuitem action='insert_citation'/>
                 </placeholder>
             </menu>
         </menubar>
@@ -63,18 +63,19 @@ class MainWindowExtension(WindowExtension):
     def __init__(self, plugin, window):
         """Window constructor."""
         WindowExtension.__init__(self, plugin, window)
-        self.window.ui.register_url_handler('zotero', self.plugin.zotero_handle)
+        self.preferences = plugin.preferences
+        self.window.ui.register_url_handler('zotero',
+                                            self.plugin.zotero_handle)
 
     @action(_('_Citation...'), '', '<Primary><Alt>I')  # T: menu item
-    def insert_citation_bbt(self):
+    def insert_citation(self):
         """Will be called by the menu item or key binding."""
-        # print dir(self.window)
-        ZoteroDialog(self.window, self.window.pageview).run()
+        ZoteroDialog(self.window, self.window.pageview, self.preferences).run()
 
 
 class ZoteroDialog(Dialog):
 
-    def __init__(self, ui, pageview):
+    def __init__(self, ui, pageview, preferences):
         Dialog.__init__(self, ui, _('Search in Zotero'),  # T: Dialog title
                         button=(_('_GO'), 'gtk-ok'),  # T: Button label
                         defaultwindowsize=(350, 200))
@@ -82,6 +83,7 @@ class ZoteroDialog(Dialog):
         self.pageview = pageview
         self.textentry = InputEntry()
         self.vbox.pack_start(self.textentry, False)
+        self.preferences = preferences
         first = None
         options = ["Search in Title, Author and Date",
                    "Search in All Fields and Tags",
@@ -102,7 +104,7 @@ class ZoteroDialog(Dialog):
         buffer = self.pageview.view.get_buffer()
         active = [r for r in self.radio.get_group() if r.get_active()]  # @+
         radiotext = active[0].get_label()  # @+
-        self.insert_citation_bbt(text, radiotext, buffer)
+        self.insert_citation(text, radiotext, buffer)
         return True
 
     def insert_citation(self, text, radiotext, buffer):
@@ -113,44 +115,32 @@ class ZoteroDialog(Dialog):
             method = '&method=fields'
         elif "Everywhere" in radiotext:
             method = '&method=everything'
-        format = '&format=bibliography'
-        url = 'http://' + root + '/search?q=' + text + format + method
-        try:
-            # resp = requests.get(url).json()
-            resp = json.loads(urllib2.urlopen(url).read())
-            for i in resp:
-                key = i['key']
-                # key = '0_' + i['id'].split('/')[-1]
-                # Sometimes, articles may have missing fields, can be skipped
-                try:
-                    href = 'zotero://' + root + '/select?key=' + key
-                    # title = i['title']
-                    bibtext = i['text']
-                    buffer.insert_link_at_cursor(bibtext, href=href)
-                    buffer.insert_at_cursor("\n")
-                except:
-                    pass
-        except:
-            pass
-
-    def insert_citation_bbt(self, text, radiotext, buffer):
-        """Will insert the BBT keys returned by zotero."""
-        root = "127.0.0.1:23119/zotxt"
-        format = '&format=betterbibtexkey'
-        method = ''  # Method defaults to titleCreatorYear
-        if "Tags" in radiotext:
-            method = '&method=fields'
-        elif "Everywhere" in radiotext:
-            method = '&method=everything'
+        style = self.preferences['bibliography_style']
+        format = '&format=' + style
         url = 'http://' + root + '/search?q=' + text + format + method
         try:
             resp = json.loads(urllib2.urlopen(url).read())
-            for bbtkey in resp:
-                try:
-                    zotlink = 'zotero://' + root + '/select?betterbibtexkey=' + bbtkey
-                    buffer.insert_link_at_cursor(bbtkey, href=zotlink)
-                    buffer.insert_at_cursor("\n")
-                except:
-                    pass
+            if style == 'bibliography':
+                for i in resp:
+                    buffer.insert_at_cursor(i)
+                    key = i['key']
+                    try:
+                        zotlink = 'zotero://' + root + '/select?key=' + key
+                        bibtext = i['text']
+                        buffer.insert_link_at_cursor(bibtext, href=zotlink)
+                        buffer.insert_at_cursor("\n")
+                    except:
+                        pass
+            elif style == 'betterbibtexkey':
+                for bbtkey in resp:
+                    try:
+                        zotlink = ('zotero://' + root +
+                                   '/select?betterbibtexkey=' + bbtkey)
+                        buffer.insert_link_at_cursor(bbtkey, href=zotlink)
+                        buffer.insert_at_cursor("\n")
+                    except:
+                        pass
+            else:
+                buffer.insert_at_cursor('style unknown: ' + style + "\n")
         except:
             pass
